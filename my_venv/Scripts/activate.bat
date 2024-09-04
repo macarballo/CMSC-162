80cb96ef8870fed88cafe4f2176559252b07dc3d
kgpelayo <kgpelayo1@up.edu.ph>
2024-09-04 20:31:01 +0800

    oho
    testing 1 2 3

--- my_venv/Scripts/activate.bat
+++ my_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Documents\4th Year\CMSC 162\Project\my_venv
+set VIRTUAL_ENV=D:\calagizing\CMSC-162\my_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
